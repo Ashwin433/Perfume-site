c2a774288bd17ab959bad1640ccc83afaf1684e9
Ashwin433 <ashwinnair332@gmail.com>
2025-12-19 23:01:01 +0530

    feat: Implement double subscription card with fragrance options and update image slider functionality

--- script/script.js
+++ script/script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const singleRadio = singleCard.querySelector('input[name="subscription"]');
   const doubleRadio = doubleCard.querySelector('input[name="subscription"]');
-
+  
   // Single selected
   singleRadio.addEventListener("change", () => {
     if (singleRadio.checked) {
@@ -22,10 +22,42 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+const sliderBtnLeft = document.querySelector('.slider-btn.left');
+const sliderBtnRight = document.querySelector('.slider-btn.right');
+const dots = document.querySelector('.sliderDts.dot')
+const mainImage = document.querySelector('#mainProductImage');
+const imagegrid = document.querySelector('#imageGrid')
 
-// image slider
+const images = [
+  "images/grid image 1.png",
+  "images/grid image 2.png",
+  "images/grid image 3.png",
+  "images/grid image 4.png"
+]
 
-const thumbnails = document.querySelectorAll(".image-grid");
-const sliderDots = document.querySelectorAll(".slider-dots");
-const images = document.querySelectorAll(".product-images")
+let currentIndex = 0;
+
+for (let i = 0; i < images.length * 2; i++) {
+  const index = i % images.length;
+
+  const img = document.createElement("img");
+  img.src = images[index];
+  img.alt = "img-thumbnail";
+  img.addEventListener("click", () => setImage(index));
+  imagegrid.appendChild(img);
+}
+
+
+
+sliderBtnLeft.addEventListener('click', () => {
+  currentIndex = ( currentIndex - 1 + images.length) %images.length;
+  setImage(currentIndex);
+  console.log('left clicked');
+});
+sliderBtnRight.addEventListener('click', () => {
+  currentIndex = ( currentIndex + 1) % images.length;
+  setImage(currentIndex);
+  console.log('right clicked'); 
+});
+
 
